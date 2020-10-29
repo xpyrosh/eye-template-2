2cdc10e4afe55008ba52b1741f8f490e8c5187ba
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-28 21:32:41 -0400

    modifying about us cards once again.... completed contact us cards, need to include form submit

--- js/app.js
+++ js/app.js
@@ -5,12 +5,22 @@ function openLocation(evt, locName){
     for (i=0; i<tabcontent.length; i++){
         tabcontent[i].style.display = "none";
     }
+    
+    // get all tablink elements and remove current class state
+    tablinks = document.getElementsByClassName('tablink');
+    for (i = 0; i < tablinks.length; i++){
+        tablinks[i].classList.remove('current');
+    }
+    
+
+    // tablinks.forEach(element => {
+    //     console.log(element);
+    //     element.classList.remove('current');
+    // });
 
     document.getElementById(locName).style.display = "block";
-}
+    // console.log(evt.currentTarget.classList);
+    evt.currentTarget.classList.add("current");
 
-// Not working
-function myFunction(){
-    // let x = document.getElementById("loc-tabs").firstChild;
-    // console.log(x);
+    evt.preventDefault();
 }
